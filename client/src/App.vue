c2c8bc12248d179e4ba4502dba59eaccbc5565e6
Jianfeng He <jianfeng.he@oliverwyman.com>
2021-08-04 18:25:25 +0800

    improve

--- client/src/App.vue
+++ client/src/App.vue
@@ -27,16 +27,11 @@
                   class="mx-2 mb-2"
                   @click="downloadBtnClick"
                   >Download
-                  <v-icon dark> mdi-download </v-icon>
+                  <v-icon class="pl-2" dark> mdi-cloud-download </v-icon>
                 </v-btn>
-                <v-btn
-                  color="primary"
-                  dark
-                  class="mb-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
+                <v-btn color="cyan" dark class="mb-2" v-bind="attrs" v-on="on">
                   New User
+                  <v-icon class="pl-2" dark> mdi-account-multiple-plus </v-icon>
                 </v-btn>
               </template>
               <v-card>
@@ -79,8 +74,12 @@
             </v-dialog>
             <v-dialog v-model="dialogDelete" max-width="500px">
               <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                  <v-icon large color="red darken-2">mdi-close-circle</v-icon>
+                  Dangerous Operation</v-card-title
+                >
                 <v-card-title class="text-h5"
-                  >Are you sure you want to delete this user?</v-card-title
+                  >Are you sure to delete this user?</v-card-title
                 >
                 <v-card-actions>
                   <v-spacer></v-spacer>
@@ -98,9 +97,10 @@
         </template>
         <!-- eslint-disable-next-line -->
         <template v-slot:item.actions="{ item }">
-          <v-icon class="pl-3" small @click="deleteItem(item)">
-            mdi-delete
-          </v-icon>
+          <v-btn color="primary" text @click="deleteItem(item)">
+            <v-icon left> mdi-delete </v-icon>
+            Delete
+          </v-btn>
         </template>
       </v-data-table>
     </v-main>
@@ -137,6 +137,7 @@ export default {
         },
         {
           text: 'Actions',
+          align: 'center',
           value: 'actions',
           sortable: false
         }
