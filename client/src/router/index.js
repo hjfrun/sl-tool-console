f5ce24d018e1ed63162a16f2c4fe42e0aecf3d16
Jianfeng He <jianfeng.he@oliverwyman.com>
2021-08-08 15:48:35 +0800

    minor

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -45,7 +45,6 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  console.log('token empty here, beforeEach')
   if (!to.meta.isPublic && !sessionStorage.token) {
     return next({ name: 'login', params: { message: 'Please login first' } })
   }
